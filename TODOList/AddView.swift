3afb79558d9474b4683150cc511370d2dfed2f64
freakyfaces <70147234+freakyfaces@users.noreply.github.com>
2022-06-11 22:30:27 +0430

    Add NavigationLink, TODOItem Class Added

---
+++ TODOList/AddView.swift
@@ -0,0 +1,15 @@
+//
+//  AddView.swift
+//  TODOList
+//
+//  Created by Sepehr Harfi on 6/11/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Binding var items: [TODOItem]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
--- TODOList/ContentView.swift
+++ TODOList/ContentView.swift
@@ -4,13 +4,57 @@
 //
 //  Created by Sepehr Harfi on 6/11/22.
 //
-
 import SwiftUI
 
+class TODOItem{
+    var total_num: Int = 0
+    var id = Int()
+    var description = ""
+    var data = Date()
+    init(description: String, data: Date){
+        self.description = description
+        self.data = data
+        self.id = total_num
+        total_num = total_num + 1
+    }
+    
+    func get_total_num() -> String{
+            return String(total_num)
+        }
+}
+
 struct ContentView: View {
+    @State var items: [TODOItem] = [
+        TODOItem(description: "Database Homework", data: Date())
+             ]
+    
+    func deleteItem(at offsets: IndexSet){
+        items.remove(atOffsets: offsets)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                //Text("Total TODOs: \(TODOItem.get_total_num())")
+                //    .bold()
+                //    .font(.title2)
+                ForEach(items, id: \.id){ item in
+                    Text(item.description)
+                        .bold()
+                        .font(.title3)
+                }
+                .onDelete(perform: deleteItem)
+            }
+            .navigationTitle("TODO List")
+            .toolbar{
+                NavigationLink {
+                    AddView(items: $items)
+                } label: {
+                    Text("Add")
+                    
+                }
+            }
+        }
     }
 }
 
