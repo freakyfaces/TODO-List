8af0ff1c9a517d1b2fa7f0ca450fd9646fa6dde3
freakyfaces <70147234+freakyfaces@users.noreply.github.com>
2022-06-11 20:46:15 +0430

    Initial Commit

---
+++ TODOList/TODOListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOListApp.swift
+//  TODOList
+//
+//  Created by Sepehr Harfi on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TODOListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
